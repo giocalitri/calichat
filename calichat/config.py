912122f112f4f46e4b0e7c46cd6dde91b5e489a0
Giovanni Di Milia <giovanni@dimilia.it>
2017-09-09 15:48:56 -0400

    Added models for rooms and messages

--- calichat/config.py
+++ calichat/config.py
@@ -11,15 +11,13 @@ from calichat.envs import (
 class Config:
     """Class for base configuration"""
 
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
     APP_VERSION = '0.0.1_alpha'
     DEBUG = get_bool('DEBUG', True)
     SECRET_KEY = get_string('SECRET_KEY', 'ls8t)o32h@bqp1s8e0&6+mepk#t4@^68yx43kjm_#tvdv=m&ke')
     SERVER_NAME = get_string('SERVER_NAME', 'calichat.local:5000')
-
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'app.db'))
-    DATABASE_CONNECT_OPTIONS = {}
-
+    SQLALCHEMY_DATABASE_URI = get_string(
+        'DATABASE_URL', 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'calichat.db')))
     CSRF_ENABLED = get_bool('CSRF_ENABLED', True)
-
     BCRYPT_LOG_ROUNDS = get_int('BCRYPT_LOG_ROUNDS', 12)
--- calichat/models.py
+++ calichat/models.py
@@ -1,27 +1,73 @@
 """Models"""
+import uuid
+
 from flask_login import UserMixin
 from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.types import TypeDecorator, CHAR
+from sqlalchemy.dialects.postgresql import UUID
 
 from calichat.extensions import db, bcrypt
 
 
+class GUID(TypeDecorator):
+    """Platform-independent GUID type.
+
+    Uses Postgresql's UUID type, otherwise uses
+    CHAR(32), storing as stringified hex values.
+
+    credits to:
+    http://docs.sqlalchemy.org/en/rel_0_9/core/custom_types.html?highlight=guid#backend-agnostic-guid-type
+    """
+    impl = CHAR
+
+    def load_dialect_impl(self, dialect):
+        if dialect.name == 'postgresql':
+            return dialect.type_descriptor(UUID())
+        else:
+            return dialect.type_descriptor(CHAR(32))
+
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return value
+        elif dialect.name == 'postgresql':
+            return str(value)
+        else:
+            if not isinstance(value, uuid.UUID):
+                return "%.32x" % uuid.UUID(value).int
+            else:
+                # hexstring
+                return "%.32x" % value.int
+
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return value
+        else:
+            return uuid.UUID(value)
+
+
 class User(db.Model, UserMixin):
-    email = db.Column(db.String(120), primary_key=True, unique=True)
+    """Model for users"""
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(120), unique=True)
     _password = db.Column(db.String(128))
 
     @hybrid_property
     def password(self):
+        """Password getter"""
         return self._password
 
     @password.setter
     def _set_password(self, plaintext):
+        """Password setter"""
         self._password = bcrypt.generate_password_hash(plaintext)
 
     def __repr__(self):
         return '<User %r>' % self.username
 
     def get_id(self):
-        """needs to be an unique string"""
+        """
+        Method for flask-login to extract the user
+        """
         return str(self.email)
 
     def is_correct_password(self, plaintext):
@@ -29,3 +75,23 @@ class User(db.Model, UserMixin):
         Checks if the password is correct
         """
         return bcrypt.check_password_hash(self._password, plaintext)
+
+
+class Room(db.Model):
+    """Model for chat rooms"""
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(120))
+
+
+class Message(db.Model):
+    """
+    Model for messages in room
+
+    NOTE: using a UUID4 as primary key to avoid risks of running out of integers.
+    A UUID4 has a risk of collition of 1/(2^64 * 16)
+    """
+    uuid = db.Column(GUID, primary_key=True)
+    message = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime)
+    room_id = db.Column(db.Integer, db.ForeignKey('room.id', ondelete='CASCADE'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
