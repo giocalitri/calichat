2dded71296b46643592da50dd9e13da9780d3ce4
Giovanni Di Milia <giovanni@dimilia.it>
2017-09-14 22:38:40 -0400

    Working on docker-compose

--- calichat/config.py
+++ calichat/config.py
@@ -16,7 +16,10 @@ class Config:
     APP_VERSION = '0.0.1_alpha'
     DEBUG = get_bool('DEBUG', False)
     SECRET_KEY = get_string('SECRET_KEY', 'ls8t)o32h@bqp1s8e0&6+mepk#t4@^68yx43kjm_#tvdv=m&ke')
-    SERVER_NAME = get_string('SERVER_NAME', 'calichat.local:5000')
+    PORT_NUMBER = get_int('PORT', 5000)
+    SERVER_BASE_NAME = get_string('SERVER_BASE_NAME', 'calichat.local')
+    SERVER_NAME = '{server_base_name}:{port_number}'.format(
+        server_base_name=SERVER_BASE_NAME, port_number=PORT_NUMBER)
     SQLALCHEMY_DATABASE_URI = get_string(
         'DATABASE_URL', 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'calichat.db')))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
